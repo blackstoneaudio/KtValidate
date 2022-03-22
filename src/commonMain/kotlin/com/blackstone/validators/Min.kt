b632c33105811dce05d5ed012dfda7e280000668
Alex Black <alex.black@blackstoneaudio.com>
2022-03-22 10:17:51 -0700

    Updates Min with >= instead of > because this makes the messaging make sense.

--- src/commonMain/kotlin/com/blackstone/validators/Min.kt
+++ src/commonMain/kotlin/com/blackstone/validators/Min.kt
@@ -8,7 +8,7 @@ class Min(val min: Int, override var message: String? = null) : Rule {
     override fun validate(input: String, fieldName: String): Boolean {
         message = message ?: "The $fieldName input was not long enough. Expected $min but got ${input.length}"
 
-        return input.length > min
+        return input.length >= min
     }
 }
 
