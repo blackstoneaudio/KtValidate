aefe51705bbdcd4cb1ec6d313e172ff95822a910
Alex Black <alex.black@blackstoneaudio.com>
2023-03-01 08:05:29 -0800

    Updates error messaging.

--- src/commonMain/kotlin/com/blackstone/validators/Max.kt
+++ src/commonMain/kotlin/com/blackstone/validators/Max.kt
@@ -6,7 +6,7 @@ import com.blackstone.Rule
 class Max(val max: Int, override var message: String? = null) : Rule {
 
     override fun validate(input: String, fieldName: String): Boolean {
-        message = message ?: "The $fieldName input was too long. Expected less than ${max + 1} but got ${input.length}"
+        message = message ?: "The $fieldName input was too long. Expected less than ${max + 1} characters but got ${input.length} characters"
 
         return input.length <= max
     }
--- src/commonMain/kotlin/com/blackstone/validators/Min.kt
+++ src/commonMain/kotlin/com/blackstone/validators/Min.kt
@@ -6,7 +6,7 @@ import com.blackstone.Rule
 class Min(val min: Int, override var message: String? = null) : Rule {
 
     override fun validate(input: String, fieldName: String): Boolean {
-        message = message ?: "The $fieldName input was not long enough. Expected $min but got ${input.length}"
+        message = message ?: "The $fieldName input was not long enough. Expected $min characters but got ${input.length} characters"
 
         return input.length >= min
     }
