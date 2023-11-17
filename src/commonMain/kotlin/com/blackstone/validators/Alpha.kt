daae3aa84b2e01d4210f886795f8cbdb02fb2658
Alex Black <alex.black@blackstoneaudio.com>
2023-11-17 15:08:55 -0800

    Adds Alpha validator. this can be used for checking passwords meet a minim requirement.

---
+++ src/commonMain/kotlin/com/blackstone/validators/Alpha.kt
@@ -0,0 +1,16 @@
+package com.blackstone.validators
+
+import com.blackstone.Rule
+
+class Alpha(private val minAlphaChars: Int, override var message: String? = null) : Rule {
+    override fun validate(input: String, fieldName: String): Boolean {
+        // Count the number of alphabetic characters (case insensitive)
+        val alphaCount = input.count { it.isLetter() }
+
+        // Update the error message with the actual count of alpha characters
+        message = message ?: "$fieldName must contain at least $minAlphaChars alphabetic characters, but only has $alphaCount"
+
+        // Check if the count of alphabetic characters meets the minimum requirement
+        return alphaCount >= minAlphaChars
+    }
+}
