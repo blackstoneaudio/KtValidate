f7935748720ed58e3a0f01d7c13a856a0c90be17
Alex Black <alex.black@blackstoneaudio.com>
2023-11-17 15:09:24 -0800

    Updates our build config with new version of kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("multiplatform") version "1.6.10"
+    kotlin("multiplatform") version "1.9.0"
     id("maven-publish")
 }
 
 group = "com.blackstone"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     google()
@@ -14,19 +14,16 @@ repositories {
 kotlin {
     jvm()
 
-//    js(IR) {
-//        browser { binaries.executable() }
-//        nodejs { binaries.executable() }
-//    }
-    val iosArm32 = iosArm32()
-    val ios64 = iosArm64()
-    val iosX64 = iosX64()
-    val iosSimArm64 = iosSimulatorArm64()
-    configure(listOf(iosArm32, ios64, iosX64, iosSimArm64)) {
-        binaries.framework {
-            baseName = "KtValidate"
-        }
+    js(IR) {
+        browser { binaries.executable() }
+        nodejs { binaries.executable() }
     }
+    watchosSimulatorArm64()
+    watchos()
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -37,22 +34,7 @@ kotlin {
         }
         val jvmMain by getting
     }
-
-
-    // Create a task to build a fat framework.
-    tasks.register<org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask>("debugFatFramework") {
-        // The fat framework must have the same base name as the initial frameworks.
-        baseName = "KtValidate"
-        // The default destination directory is "<build directory>/fat-framework".
-        destinationDir = buildDir.resolve("KtValidate/debug")
-        // Specify the frameworks to be merged.
-        from(
-            iosX64.binaries.getFramework("DEBUG"),
-            ios64.binaries.getFramework("DEBUG")
-        )
-    }
-
-
+    
 }
 
 publishing {
@@ -60,16 +42,16 @@ publishing {
     val mavenUser: String by project
     val mavenPassword: String by project
 
-//    repositories {
-//        maven {
-//            setUrl("https://repos.awhb.dev/releases")
-//            authentication {
-//                create("basic", BasicAuthentication::class.java)
-//            }
-//            credentials {
-//                username = mavenUser
-//                password = mavenPassword
-//            }
-//        }
-//    }
+    repositories {
+        maven {
+            setUrl("https://repos.awhb.dev/releases")
+            authentication {
+                create("basic", BasicAuthentication::class.java)
+            }
+            credentials {
+                username = mavenUser
+                password = mavenPassword
+            }
+        }
+    }
 }
