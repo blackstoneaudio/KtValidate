827464a8b82dcd5c991d2dd27ed4d9c15af4439f
Alex Black <alex.black@blackstoneaudio.com>
2023-11-17 15:09:10 -0800

    Adds SpecialCharacter validator. this can be used for checking passwords meet a minimum requirement.

---
+++ src/commonMain/kotlin/com/blackstone/validators/SpecialCharacter.kt
@@ -0,0 +1,17 @@
+package com.blackstone.validators
+
+import com.blackstone.Rule
+
+class SpecialCharacter(private val minSpecialCharacter: Int, override var message: String? = null) :
+    Rule {
+    override fun validate(input: String, fieldName: String): Boolean {
+        // Count the number of non-alphanumeric characters
+        val specialCharacterCount = input.count { !it.isLetterOrDigit() }
+
+        // Update the error message with the actual count of non-alphanumeric characters
+        message = message ?: "$fieldName must contain at least $minSpecialCharacter non-alphanumeric characters, but only has $specialCharacterCount"
+
+        // Check if the count of non-alphanumeric characters meets the minimum requirement
+        return specialCharacterCount >= minSpecialCharacter
+    }
+}
