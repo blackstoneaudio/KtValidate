0a0599285446687a2150f176f7088c1723b2fe91
Alex Black <alex.black@blackstoneaudio.com>
2023-11-17 15:22:27 -0800

    More messaging cleanup for SpecialCharacter validator

--- src/commonMain/kotlin/com/blackstone/validators/SpecialCharacter.kt
+++ src/commonMain/kotlin/com/blackstone/validators/SpecialCharacter.kt
@@ -9,7 +9,7 @@ class SpecialCharacter(private val minSpecialCharacter: Int, override var messag
         val specialCharacterCount = input.count { !it.isLetterOrDigit() }
 
         // Update the error message with the actual count of non-alphanumeric characters
-        message = message ?: "$fieldName must contain at least $minSpecialCharacter non-alphanumeric characters example:!@#$%^&*(), but has $specialCharacterCount"
+        message = message ?: "$fieldName must contain at least $minSpecialCharacter non-alphanumeric characters, but has $specialCharacterCount. example:!@#$%^&*()"
 
         // Check if the count of non-alphanumeric characters meets the minimum requirement
         return specialCharacterCount >= minSpecialCharacter
