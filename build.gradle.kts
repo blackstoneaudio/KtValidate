4b81a7266619baf62148c464de14cffbdec79d95
Alex Black <alexwhb@gmail.com>
2024-12-17 15:42:02 -0800

    FOR BETA RELEASE ONLY!!! DO NOT MERGE! This configures our android and iOS beta apps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.5.0"
+    kotlin("multiplatform") version "1.9.0"
     id("maven-publish")
 }
 
@@ -13,37 +13,26 @@ repositories {
 
 kotlin {
     jvm()
-    val ios64 = iosArm64("ios64")
-    val iosX64 = iosX64("iosX64")
-    configure(listOf(ios64, iosX64)) {
-        binaries.framework {
-            baseName = "KtValidate"
-        }
-    }
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val jvmMain by getting
-    }
+
+    watchosSimulatorArm64()
+    watchos()
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
 
 
     // Create a task to build a fat framework.
-    tasks.register<org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask>("debugFatFramework") {
-        // The fat framework must have the same base name as the initial frameworks.
-        baseName = "KtValidate"
-        // The default destination directory is "<build directory>/fat-framework".
-        destinationDir = buildDir.resolve("KtValidate/debug")
-        // Specify the frameworks to be merged.
-        from(
-            iosX64.binaries.getFramework("DEBUG"),
-            ios64.binaries.getFramework("DEBUG")
-        )
-    }
+//    tasks.register<org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask>("debugFatFramework") {
+//        // The fat framework must have the same base name as the initial frameworks.
+//        baseName = "KtValidate"
+//        // The default destination directory is "<build directory>/fat-framework".
+//        destinationDir = buildDir.resolve("KtValidate/debug")
+//        // Specify the frameworks to be merged.
+//        from(
+//            iosX64.binaries.getFramework("DEBUG"),
+//            ios64.binaries.getFramework("DEBUG")
+//        )
+//    }
 
 
 }
